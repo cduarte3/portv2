be5cfc4eff5ae8c1498e65183000da7c47492c57
Christian Duarte <cduarte3@myseneca.ca>
2024-05-18 14:54:34 -0400

    star rating radios and updated feedback

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import BasicRating from "./radio";
 
 export default function Feedback() {
   return (
@@ -22,7 +23,7 @@ export default function Feedback() {
           className="p-[30px] block w-full max-h-screen text-lg text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           placeholder="Leave a comment..."
         ></textarea>
-        
+        <BasicRating />
       </form>
     </div>
   );
